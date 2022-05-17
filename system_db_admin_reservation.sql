03b225b8d9303498cbf29c12cf1c68e7b19d4206
Jacobi <92586621+blurmemo@users.noreply.github.com>
2022-05-17 11:57:22 +0800

    测试数据
    直接以SQL脚本方式导入system_db数据库

---
+++ system_db_admin_reservation.sql
@@ -0,0 +1,7 @@
+insert into system_db.admin_reservation (classroomId, teacherId, teacherName, submitTime, reserveTime, state) values ('4203', '2394792', '魏玲', '2022-05-10 16:03:00', '17:20', '不通过');
+insert into system_db.admin_reservation (classroomId, teacherId, teacherName, submitTime, reserveTime, state) values ('1309', '2394792', '魏玲', '2022-05-10 16:26:20', '13:20', '不通过');
+insert into system_db.admin_reservation (classroomId, teacherId, teacherName, submitTime, reserveTime, state) values ('1207', '2394792', '魏玲', '2022-05-10 16:26:21', '13:40', '通过');
+insert into system_db.admin_reservation (classroomId, teacherId, teacherName, submitTime, reserveTime, state) values ('4203', '2394792', '魏玲', '2022-05-10 16:26:23', '13:20', '待审核');
+insert into system_db.admin_reservation (classroomId, teacherId, teacherName, submitTime, reserveTime, state) values ('1104', '2394792', '魏玲', '2022-05-10 16:26:24', '14:30', '待审核');
+insert into system_db.admin_reservation (classroomId, teacherId, teacherName, submitTime, reserveTime, state) values ('1207', '2394792', '魏玲', '2022-05-10 16:26:25', '15:40', '待审核');
+insert into system_db.admin_reservation (classroomId, teacherId, teacherName, submitTime, reserveTime, state) values ('4203', '2134533', '黄二', '2022-05-16 18:44:42', '13:20', '待审核');
