1a240c6654b64c6d2753928aed1bfad478e542de
Jacobi <92586621+blurmemo@users.noreply.github.com>
2022-05-17 10:21:35 +0800

    Delete system_db_user_admin.sql

--- system_db_user_admin.sql
+++
@@ -1,4 +0,0 @@
-insert into system_db.user_admin (username, password) values ('2157127', '123456');
-insert into system_db.user_admin (username, password) values ('2157128', '123345');
-insert into system_db.user_admin (username, password) values ('2157129', '112347');
-insert into system_db.user_admin (username, password) values ('2036106', '044529');
